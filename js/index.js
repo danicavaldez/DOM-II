d206e41704feebc69622789d2e270975c6d24d18
Danica Valdez <danicavaldez@Danicas-MacBook-Pro.local>
2019-06-08 10:46:02 -0700

    Prevented even propagation

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@
 const funBus = document.querySelector(".logo-heading");
 const navItems = document.querySelectorAll("nav a");
 const navigationBar = document.querySelector(".main-navigation");
+const intro = document.querySelector(".intro h2");
+const button = document.querySelector(".btn");
+const signMeUp = document.querySelector(".content-pick");
 
 funBus.addEventListener("mouseover", () => {
   funBus.classList.add("uppercase");
@@ -9,7 +12,7 @@ funBus.addEventListener("mouseover", () => {
 
 window.addEventListener("scroll", () => {
   console.log("Scrolling, scrolling, scrolling.")
-})
+});
 
 navigationBar.addEventListener("dblclick", () => {
   navigationBar.style.backgroundColor = "peachpuff";
@@ -21,3 +24,17 @@ navItems.forEach((link) =>
     console.log('Default prevented!');
   })
 );
+
+intro.addEventListener("wheel", () => {
+  intro.style.fontSize = "8.5rem";
+  intro.style.color = "gold"
+});
+
+signMeUp.addEventListener("click", () => {
+  signMeUp.style.backgroundColor = "palevioletred";
+})
+
+button.addEventListener("click", () => {
+  button.style.backgroundColor = "lavender";
+  event.stopPropagation();
+});
