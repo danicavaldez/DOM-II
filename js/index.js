f327d5431c958999ef40db8862f22fdd2fa80742
Danica Valdez <danicavaldez@Danicas-MacBook-Pro.local>
2019-06-08 10:28:53 -0700

    finished 3 event listeners and prevented refresh from nav items

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // Your code goes here
+const funBus = document.querySelector(".logo-heading");
+const navItems = document.querySelectorAll("nav a");
+const navigationBar = document.querySelector(".main-navigation");
+
+funBus.addEventListener("mouseover", () => {
+  funBus.classList.add("uppercase");
+});
+
+window.addEventListener("scroll", () => {
+  console.log("Scrolling, scrolling, scrolling.")
+})
+
+navigationBar.addEventListener("dblclick", () => {
+  navigationBar.style.backgroundColor = "peachpuff";
+});
+
+navItems.forEach((link) =>
+  link.addEventListener('click', () => {
+    link.preventDefault;
+    console.log('Default prevented!');
+  })
+);
